f4f6b41bb1d466db314581f4c3977f068ae7d879
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2023-08-13 16:03:49 +0900

    Add ctrlc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use v4l::buffer::Type;
 use v4l::io::mmap::Stream;
 use v4l::io::traits::CaptureStream;
@@ -6,6 +8,13 @@ use v4l::Device;
 use v4l::FourCC;
 
 fn main() {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
     let dev = Device::new(0).expect("Failed to open device");
 
     let mut fmt = dev.format().expect("Failed to read format");
@@ -16,18 +25,23 @@ fn main() {
 
     // The actual format chosen by the device driver may differ from what we
     // requested! Print it out to get an idea of what is actually used now.
-    println!("Format in use:\n{}", fmt);
+    eprintln!("Format in use:\n{}", fmt);
 
     let mut stream =
         Stream::with_buffers(&dev, Type::VideoCapture, 4).expect("Failed to create buffer stream");
 
-    loop {
-        let (buf, meta) = stream.next().unwrap();
-        println!(
-            "Buffer size: {}, seq: {}, timestamp: {}",
-            buf.len(),
-            meta.sequence,
-            meta.timestamp
-        );
+    while running.load(Ordering::SeqCst) {
+        match stream.next() {
+            Ok(t) => {
+                let (buf, meta) = t;
+                eprintln!(
+                    "Buffer size: {}, seq: {}, timestamp: {}",
+                    buf.len(),
+                    meta.sequence,
+                    meta.timestamp
+                );
+            }
+            Err(e) => eprintln!("e = {}", e),
+        }
     }
 }
