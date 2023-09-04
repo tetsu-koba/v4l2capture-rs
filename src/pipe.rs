45437d33ea431d66758ce13aa91931f58f9e2cf4
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-09-04 12:56:07 +0900

    Use std::fs::read_to_string to simplify

--- src/pipe.rs
+++ src/pipe.rs
@@ -1,9 +1,7 @@
 use nix::errno::Errno;
 use nix::fcntl::{fcntl, FcntlArg};
 use nix::fcntl::{vmsplice, SpliceFFlags};
-use std::fs::File;
-use std::io::IoSlice;
-use std::io::{self, Read};
+use std::io::{self, IoSlice};
 use std::os::unix::io::RawFd;
 
 // Check if the given file descriptor is a pipe
@@ -18,11 +16,8 @@ pub fn is_pipe(fd: RawFd) -> bool {
 #[cfg(target_os = "linux")]
 pub fn get_pipe_max_size() -> Result<usize, io::Error> {
     // Read the maximum pipe size
-    let mut pipe_max_size_file = File::open("/proc/sys/fs/pipe-max-size")?;
-    let mut buffer = String::new();
-    pipe_max_size_file.read_to_string(&mut buffer)?;
-    let max_size_str = buffer.trim_end();
-    let max_size: usize = max_size_str.parse().map_err(|err| {
+    let pipe_max_size = std::fs::read_to_string("/proc/sys/fs/pipe-max-size")?;
+    let max_size: usize = pipe_max_size.trim_end().parse().map_err(|err| {
         eprintln!("Failed to parse /proc/sys/fs/pipe-max-size: {:?}", err);
         io::Error::new(io::ErrorKind::InvalidData, "Failed to parse max pipe size")
     })?;
